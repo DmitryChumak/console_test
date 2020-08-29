784f1f3b54d4b11749d10ead52fe1fe2a76ef989
LAN\student <nikitatututu69@gmail.com>
2020-08-29 17:40:13 +0300

    Added ListForm that can show files and return them

---
+++ TestApp/TestApp/ListForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class ListForm : Form
+    {
+        string path;
+        string searchPattern;
+        FileInfo[] files;
+
+        public ListForm(string directory, string type)
+        {
+            InitializeComponent();
+            this.path = directory;
+            this.searchPattern = type;
+        }
+
+        private void ListForm_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            files = directoryInfo.GetFiles(searchPattern);
+            listBox1.DataSource = files;
+        }
+        public FileInfo[] GetItems()
+        {
+            return files;
+        }
+    }
+}
