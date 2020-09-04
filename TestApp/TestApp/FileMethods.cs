fe9f50d365c189e6c211547e5eb289f7666928a5
VyacheslavDzhus <slavadzhus158@gmail.com>
2020-09-04 23:50:27 +0300

    Added FileMamager

---
+++ TestApp/TestApp/FileMethods.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace TestApp
+{
+     public class FileMethods
+    {
+        public void Save_Txt(string filePath, string data)
+        {
+            File.AppendAllText(filePath, data);
+        }
+        public void Save_Dat(string filePath, string data)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+            bf.Serialize(fs, data);
+            fs.Close();
+        }
+        public void Save_Json(string filePath, string data)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.AppendAllText(filePath, json);
+        }
+        public void Save_Xml(string filePath, string data)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(string));
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, data);
+            }
+        }
+
+        public string Open_Txt(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        public string Open_Dat(string filePath)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                return bf.Deserialize(fs) as string;
+                
+            }
+            
+        }
+
+        public string Open_Json(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject(json) as string;
+        }
+
+        public string Open_Xml(string filePath)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(string));
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                return (string)formatter.Deserialize(fs);
+            }
+        }
+    }
+}
---
+++ TestApp/TestApp/RepositoryFileManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace TestApp
+{
+    class RepositoryFileManager 
+    {
+        private Dictionary<string, Action<FileMethods>> saveFileManipulations =
+            new Dictionary<string, Action<FileMethods>>();
+        
+        private Dictionary<string, Func<string, string>> openFileManipulations =
+            new Dictionary<string, Func<string, string>>();
+       
+        public void SaveToFile(string data)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            FileMethods fm = new FileMethods();
+
+            sfd.Filter = "txt(*.txt) | *.txt|dat(*.dat) | *.dat|json(*.json) | *.json|xml(*.xml) | *.xml";
+            sfd.ShowDialog();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                saveFileManipulations.Add(".txt", (method) => method.Save_Txt(sfd.FileName, data));
+                saveFileManipulations.Add(".dat", (method) => method.Save_Dat(sfd.FileName, data));
+                saveFileManipulations.Add(".json", (method) => method.Save_Json(sfd.FileName, data));
+                saveFileManipulations.Add(".xml", (method) => method.Save_Xml(sfd.FileName, data));                            
+                saveFileManipulations[Path.GetExtension(sfd.FileName)](fm);
+            }           
+        }
+
+        public string ReadFromFile()
+        {
+            string text = null;
+            OpenFileDialog ofd = new OpenFileDialog();
+            FileMethods fm = new FileMethods();
+            ofd.Filter = "txt(*.txt) | *.txt|dat(*.dat) | *.dat|json(*.json) | *.json|xml(*.xml) | *.xml";
+            ofd.ShowDialog();
+        
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                openFileManipulations.Add(".txt", fm.Open_Txt);
+                openFileManipulations.Add(".dat", fm.Open_Dat);
+                openFileManipulations.Add(".json", fm.Open_Json);
+                openFileManipulations.Add(".xml", fm.Open_Xml);
+                text = openFileManipulations[Path.GetExtension(ofd.FileName)](ofd.FileName);
+            }
+            return text;
+        }
+            
+      
+    }
+}
